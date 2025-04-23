24aefc7cd8630778872e32e1cea18e7a58bd509f
Kshesho <wowplayer808@yahoo.com>
2025-04-23 19:55:12 -0400

    Wow. The first and only user commit. How lonely.

---
+++ Assets/bullet.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public BoxCollider2D col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Bla());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * 4 * Time.deltaTime);
+        Vector2 pos = transform.position;
+        if (transform.position.x < -10)
+        {
+            transform.position = new Vector2(10, pos.y);
+        }
+        else if (transform.position.x > 10)
+        {
+            transform.position = new Vector2(- 10, pos.y);
+        }
+        else if (transform.position.y < -6)
+        {
+            transform.position = new Vector2(pos.x, 6);
+        }
+        else if (transform.position.y > 6)
+        {
+            transform.position = new Vector2(pos.x, -6);
+        }
+    }
+
+    IEnumerator Bla()
+    {
+        yield return new WaitForSeconds(0.5f);
+        col.enabled = true;
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+   public GameObject bullet;
+    int spawnInt, score;
+    static int highScore;
+    public Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText.text = "High Score: " + highScore;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        transform.Translate(new Vector2(h, v) * 5 * Time.deltaTime);
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, -9, 9),
+            Mathf.Clamp(transform.position.y, -4.5f, 5.5f));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            switch (spawnInt)
+            {
+                case 0:
+                    Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, 0));
+                    break;
+                case 1:
+                    Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, -90));
+                    break;
+                case 2:
+                    Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, 90));
+                    break;
+                case 3:
+                    Instantiate(bullet, transform.position, Quaternion.Euler(0, 0, 180));
+                    break;
+            }
+            spawnInt++;
+            if (spawnInt > 3)
+            {
+                spawnInt = 0;
+            }
+
+            score++;
+            if (score > highScore)
+            {
+                highScore = score;
+                scoreText.text = "High Score: " + highScore;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
